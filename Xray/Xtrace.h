fb09796bce37cd7a184e0fe78b9445167bd8fe97
John Holdsworth <github@johnholdsworth.com>
2014-03-25 16:07:56 +0000

    An obscure exception for 64 bits.

--- Xray/Xtrace.h
+++ Xray/Xtrace.h
@@ -7,7 +7,7 @@
 //
 //  Repo: https://github.com/johnno1962/Xtrace
 //
-//  $Id: //depot/Xtrace/Xray/Xtrace.h#30 $
+//  $Id: //depot/Xtrace/Xray/Xtrace.h#31 $
 //
 //  Class to intercept messages sent to a class or object.
 //  Swizzles generic logging implemntation in place of the
@@ -60,7 +60,7 @@
 #endif
 
 #define XTRACE_EXCLUSIONS \
-    "^(allocWithZone:|initWithCoder:|_UIAppearance_|_hasBaseline|"\
+    "^(allocWithZone:|initWithCoder:|_UIAppearance_|_hasBaseline|_tryRetain|"\
     "_(initializeFor|performUpdatesForPossibleChangesOf)Idiom:|"\
     "timeIntervalSinceReferenceDate)|WithObjects(AndKeys)?:$"
 
